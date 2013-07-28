692f944965e46203f1632c1c9eb3af96e13b1398
James W Grenning <james@grenning.net>
2013-07-28 17:33:26 -0500

    Mocking io_write().  Experiment next with the mocks and call order

--- io_mock.c
+++ io_mock.c
@@ -11,14 +11,27 @@ io_data io_read(io_address offset)
     return (int)mock();
 }
 
+void  io_write(io_address offset, io_data data)
+{
+    check_expected(offset);
+    check_expected(data);
+}
+
 static void test_mock_io_read(void **state) {
     expect_value(io_read, offset, 0xdead);
     will_return(io_read, 0x42);
     assert_int_equal(0x42, io_read(0xdead));
 }
 
+static void test_mock_io_write(void **state) {
+    expect_value(io_write, offset, 0xdead);
+    expect_value(io_write, data, 0xbeef);
+    io_write(0xdead, 0xbeef);
+}
+
 static const UnitTest tests[] = {
     unit_test(test_mock_io_read),
+    unit_test(test_mock_io_write),
 };
 
 int run_io_mock_tests() {
