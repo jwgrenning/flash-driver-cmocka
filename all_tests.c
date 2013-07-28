df55264dbd418758e692fa275897315ba279bb4d
James W Grenning <james@grenning.net>
2013-07-28 17:41:01 -0500

    During the experiments, I noticed that error counts are not accumulated if 'run_tests()' is called twice. So I added a test summary.

--- all_tests.c
+++ all_tests.c
@@ -8,8 +8,16 @@ int run_assert_tests(void);
 int run_io_mock_tests(void);
 
 int main(void) {
-    return  run_io_mock_tests()
-            + run_assert_tests();
+    int total_failures =
+            run_assert_tests() +
+            run_io_mock_tests();
+
+    if (total_failures == 0)
+        print_message("\nOK - NO TEST FAILURES\n");
+    else if (total_failures == 1)
+        print_message("\nERROR, 1 TEST FAILED\n");
+    else
+        print_message("\nERROR, %i TESTS FAILED\n", total_failures);
+
+    return total_failures;
 }
-
-
