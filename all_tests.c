c526ba3a2c12db3f76a35033f5afa513415e5505
James W Grenning <james@grenning.net>
2013-07-28 17:03:57 -0500

    Experimenting with the simple asserts in cmocka

--- all_tests.c
+++ all_tests.c
@@ -3,12 +3,25 @@
 #include <stddef.h>
 #include <setjmp.h>
 #include <cmocka.h>
+#include <stdbool.h>
 
-static void test_success(void **state) {
+static void test_simple_asserts(void **state) {
+    assert_false(false);
+    assert_in_range(10, 9, 11);
+    assert_int_equal(99, 99);
+    assert_int_not_equal(42, 43);
+    assert_memory_equal("this", "this", 4);
+    assert_memory_not_equal("this", "that", 4);
+    assert_non_null(test_simple_asserts);
+    assert_not_in_range(999, 1, 100);
+    assert_null(NULL);
+    assert_string_equal("hey", "hey");
+    assert_string_not_equal("hey", "hay");
+    assert_true(true);
 }
 
 static const UnitTest tests[] = {
-        unit_test(test_success),
+        unit_test(test_simple_asserts),
 };
 
 int main(void) {
