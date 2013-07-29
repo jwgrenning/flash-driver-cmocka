5bf7386ec41b8ab50a799112943c34056a0e0475
James W Grenning <james@grenning.net>
2013-07-29 15:00:42 -0500

    Extracted expect_io_read() helper function

--- flash_tests.c
+++ flash_tests.c
@@ -12,6 +12,12 @@ static void expect_io_write(void **state, io_address addr, io_data data)
     expect_value(io_write, data, data);
 }
 
+static void expect_io_read(void **state, io_address addr, io_data value_to_return)
+{
+    expect_value(io_read, offset, addr);
+    will_return(io_read, value_to_return);
+}
+
 static void expect_enter_programming_mode(void **state)
 {
     expect_io_write(state, 0, 0x40);
@@ -20,8 +26,7 @@ static void expect_enter_programming_mode(void **state)
 
 static void test_program_succeeds_ready_immediately(void **state) {
 	expect_enter_programming_mode(state);
-	expect_value(io_read, offset, 0);
-    will_return(io_read, 1<<7);
+	expect_io_read(state, 0, 1<<7);
 
     assert_int_equal(FLASH_SUCCESS, flash_program(0xdead, 0xbeef));
 }
